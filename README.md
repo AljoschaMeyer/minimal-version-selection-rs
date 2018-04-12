8c67c96e27345a7fd49cf18af057614e944054fb
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-04-12 12:42:57 +0200

    Add metadata

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minimal Version Selection
+
+An implementation of [minimal-version-selection](https://research.swtch.com/vgo-mvs) in rust.
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! TODO
+#![deny(missing_docs)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
